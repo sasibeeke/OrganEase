b075a957d729eaed56169dd747bd422318a9cbbb
sasibeeke <42212652+sasibeeke@users.noreply.github.com>
2025-06-22 19:41:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use("/api/order", orderRouter);
 app.use("/api/customize", customizeRouter);
 
 // Run Server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
 });
